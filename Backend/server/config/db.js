b5a912fd6efd2169c972f4c08b9ebd3eb6b72ac8
bhawna <bhawnakalsi4@gmail.com>
2025-05-27 10:38:40 +0530

    db

--- Backend/server/config/db.js
+++ Backend/server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose")
 
 // mongoose.connect("mongodb://127.0.0.1:27017/strayToStayDb")
-mongoose.connect(process.env.MONGO_STR)
+mongoose.connect("mongodb+srv://bhawnakalsi4:wZWJpM745K3fMoHo@cluster0.we9tivn.mongodb.net/strayToStayDb")
 .then(()=>{
     console.log("Database is connected!!");
 })
